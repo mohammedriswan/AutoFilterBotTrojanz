bf85cd5d34a69b3d57c6cfc3026de25b9571eeff
anandhakrishnanlop <83494458+anandhakrishnanlop@users.noreply.github.com>
2021-05-02 11:56:24 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -167,9 +167,9 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         elif query.data == "start_data":
             await query.answer()
             keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("HELP", callback_data="help_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("HELP", url="https://t.me/TroJanzSupport"),
+                    InlineKeyboardButton("ABOUT", url="https://t.me/TroJanzSupport")],
+                
             ])
 
             await query.message.edit_text(
@@ -182,9 +182,9 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         elif query.data == "help_data":
             await query.answer()
             keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("BACK", callback_data="start_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")]
+                [InlineKeyboardButton("BACK", url="https://t.me/TroJanzSupport"),
+                    InlineKeyboardButton("ABOUT", url="https://t.me/TroJanzSupport")],
+
             ])
 
             await query.message.edit_text(
@@ -199,7 +199,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")]
+
             ])
 
             await query.message.edit_text(
